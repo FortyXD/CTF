2e96c8d0e8f2c30df27cc2ce54c9ce5acb1163ab
andrey <mais06062005@gmail.com>
2021-07-14 18:36:38 +0300

    .

--- level6/script.js
+++ level6/script.js
@@ -3,7 +3,7 @@ document.getElementById("cluc").onclick = function() {myFunction()};
 function myFunction() {
     let a= document.getElementById("text").value
 
-  window.location.href="../index.html";
+
     if (a==="text"){
         alert("Correct")
     }
--- level7/script.js
+++ level7/script.js
@@ -3,7 +3,7 @@ document.getElementById("cluc").onclick = function() {myFunction()};
 function myFunction() {
     let a= document.getElementById("text").value
 
-  window.location.href="../index.html";
+
     if (a==="text"){
         alert("Correct")
         window.location.replace('../level2/level.html');
